f56f25b60abaa73e6b8764035d95cb5710f94db2
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-10-05 02:03:36 +0530

    contract: added LetsPlay contract, chainlink random
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- smart_contract/contracts/LetsPlay.sol
+++ smart_contract/contracts/LetsPlay.sol
@@ -1,17 +1,123 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.7;
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./VRFv2Consumer.sol";
 
-contract LetsPlay is ERC20, Ownable {
+contract LetsPlay is VRFv2Consumer {
 
+    //
     uint256 public maxPlayers;
+    uint256 public constant entryFee = 0.0000000001 ether;
+    uint256 public gameCount;
 
-    constructor() ERC20("","") Ownable()
-    {
+
+    constructor(uint64 _subscriptionId) VRFv2Consumer(_subscriptionId) {
         maxPlayers = 2;
     }
 
 
+    Game[] public games;
+
+
+    struct Game {
+        uint256 createdDate;
+        bool isStarted;
+        bool isEnded;
+        address winner;
+        uint256 winnerAmount;
+        uint256 presentPlayersCount;
+        address[] players;
+    }
+
+
+    function createGame() external {
+        gameCount += 1;
+
+        address[] memory _dummyPlayers;
+
+        Game memory _game = Game({
+            createdDate: block.timestamp,
+            isStarted: false,
+            isEnded: false,
+            winner: address(0),
+            winnerAmount: 0,
+            presentPlayersCount: 0,
+            players: _dummyPlayers
+        });
+
+        games.push(_game);
+    }
+
+
+    function stakeGameFee(uint256 _gameId) external payable returns(uint256 _requestId, bool isLast) {
+        require(games[_gameId].isEnded == false, "Game Ended Started");
+        require(games[_gameId].isStarted == false, "Game is currently running");
+        require(
+            games[_gameId].presentPlayersCount < maxPlayers,
+            "Max Players Exceeded"
+        );
+        require(msg.value > entryFee, "Entry Fee is too low");
+        games[_gameId].presentPlayersCount += 1;
+
+        if (games[_gameId].presentPlayersCount == maxPlayers) {
+            games[_gameId].isStarted = true;
+            games[_gameId].winnerAmount = entryFee * maxPlayers;
+            _requestId =  requestRandomWords();
+            isLast = true;
+        } else {
+            games[_gameId].players.push(msg.sender);
+        }
+    }
+
+    function claimStake(uint256 _gameId, uint256 _requestId) external
+    {
+        require(games[_gameId].isEnded, "Already Claimed");
+        bool fulfillDone = false;
+        address payable _winnerAddress;
+        address temp_winnerAddress;
+        (temp_winnerAddress, fulfillDone) = winner(_gameId, _requestId);
+
+        if(fulfillDone)
+        {
+            _winnerAddress = payable(temp_winnerAddress);
+            delete games[_gameId].players;
+            games[_gameId].isEnded = true;
+            _winnerAddress.transfer(entryFee * maxPlayers);
+            games[_gameId].winnerAmount = entryFee * maxPlayers;
+            games[_gameId].winner= temp_winnerAddress;
+        }
+    }
+
+    function winner(uint256 _gameId, uint256 _requestId) public view returns(address _winnerAddress,bool fulfillDone)
+    {
+        uint256[] memory data;
+        uint256 _ans;
+        (fulfillDone, data)  =  getRequestStatus(_requestId);
+        if(fulfillDone)
+        {
+            _ans = data[0];
+        }
+        _ans = _ans%maxPlayers;
+        _winnerAddress = games[_gameId].players[_ans];
+        return (_winnerAddress, fulfillDone);
+    }
+
+
+    // once last player had joined the game automatically game will be started
+    function hasAllPlayersStaked(uint256 _gameId) public view returns (bool) {
+        return games[_gameId].presentPlayersCount == maxPlayers;
+    }
+
+
+    function hasUserPaid(uint256 _gameId) public view returns (bool _isPaid) {
+        uint256 _playersSize = games[_gameId].players.length;
+
+        for (uint256 i = 0; i < _playersSize; i++) {
+            if (games[_gameId].players[i] == msg.sender) {
+                _isPaid = true;
+            }
+        }
+        _isPaid = false;
+        return _isPaid;
+    }
 }
