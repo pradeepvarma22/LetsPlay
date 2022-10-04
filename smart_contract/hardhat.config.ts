a00488b0748f13a72b4712506f15b6a510a4516d
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-10-04 23:57:24 +0530

    contract: hardhat config setup new paths
    Signed-off-by: pradeepvarma22 <pradeepnadimpalli22@gmail.com>

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -2,7 +2,20 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17"
+  },
+  paths: {
+    artifacts: "./data/src/artifacts",
+    cache: "./data/src/cache"
+  },
+  typechain: {
+    outDir: "./data/src/types"
+  },
+  etherscan: {
+    apiKey: process.env.ETHER_SCAN_API_KEY!
+  }
+
 };
 
 export default config;
